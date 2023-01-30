839f1fb292eee3eabecea8ed7f25aa62a280ef0e
Eric Castro <castro.eric@hotmail.com>
2023-01-30 16:49:06 -0300

    application creation

---
+++ src/main/java/com/example/wordlinetestapi/WordlineTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordlinetestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlineTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordlineTestApiApplication.class, args);
+	}
+
+}
