b563f3ed1e32a1dc27e386fc6a1a8900faa3f90e
Eric Castro <castro.eric@hotmail.com>
2023-01-30 18:06:34 -0300

    implementation of controller of perfect numbers

---
+++ src/main/java/com/example/wordlinetestapi/controller/PerfectNumberController.java
@@ -0,0 +1,28 @@
+package com.example.wordlinetestapi.controller;
+
+import com.example.wordlinetestapi.service.PerfectNumberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@RestController
+@RequestMapping("/perfect-number")
+public class PerfectNumberController {
+
+    private PerfectNumberService service;
+
+    public PerfectNumberController(PerfectNumberService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<List<BigInteger>> getPerfectNumbers(@RequestBody BigInteger[] numbers){
+        List<BigInteger> response = service.getPerfectNumbers(numbers);
+        return ResponseEntity.ok(response);
+    }
+}
